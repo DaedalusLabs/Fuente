ae441447fb150efb992767c2d753d04b7e092501
Diego Pardo <engineer.pardo@gmail.com>
2024-12-10 15:35:09 -0600

    Templates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,12 +11,16 @@ module.exports = {
                 'fuente-light': '#2aa1e2',
                 'fuente': '#4167e8',
                 'fuente-dark': '#3b1197',
-
+                'fuente-forms': "#3859c7",
+                "fuente-buttons": "#6fe5fa"
             },
             fontFamily: {
                 'product': ['OpenSans', 'sans-serif'],
                 'mplus': ['MPlus', 'sans-serif'],
             },
+            backgroundImage: {
+                logo: `url('/templates/src/img/Logo Fuente.jpeg')`
+            }
         },
     },
 };
