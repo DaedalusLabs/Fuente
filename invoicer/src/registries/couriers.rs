740539bbe154d681b0c245941c14e8414fbd3d12
42pupusas <technology@illuminodes.com>
2024-12-19 17:07:29 -0600

    fixing channel overuse

--- invoicer/src/registries/couriers.rs
+++ invoicer/src/registries/couriers.rs
@@ -1,42 +1,41 @@
+use fuente::models::DriverProfile;
 use nostro2::notes::NostrNote;
 use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub struct CourierRegistry {
-    consumers: HashMap<String, CourierRegistryEntry>,
+    couriers: HashMap<String, CourierRegistryEntry>,
 }
 impl Default for CourierRegistry {
     fn default() -> Self {
         Self {
-            consumers: HashMap::new(),
+            couriers: HashMap::new(),
         }
     }
 }
 impl CourierRegistry {
-    pub fn is_registered(&self, consumer_id: &str) -> bool {
-        self.consumers.contains_key(consumer_id)
+    pub fn is_registered(&self, courier_id: &str) -> bool {
+        self.couriers.contains_key(courier_id)
     }
-    pub fn insert_courier(&mut self, consumer_id: String, entry: CourierRegistryEntry) {
-        self.consumers.insert(consumer_id, entry);
+    pub fn insert_courier(&mut self, courier_id: String, entry: CourierRegistryEntry) {
+        self.couriers.insert(courier_id, entry);
     }
-    pub fn update_blacklisted(&mut self, blacklist: &Vec<String>) {
-        self.consumers.iter_mut().for_each(|(id, entry)| {
-            entry.blacklisted = blacklist.contains(id);
-        });
+    pub fn find_courier(&self, courier_id: &str) -> Option<NostrNote> {
+        self.couriers
+            .get(courier_id)
+            .map(|entry| entry.profile.clone())
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct CourierRegistryEntry {
     pub profile: NostrNote,
-    pub blacklisted: bool,
 }
 
 impl Default for CourierRegistryEntry {
     fn default() -> Self {
         Self {
             profile: NostrNote::default(),
-            blacklisted: false,
         }
     }
 }
